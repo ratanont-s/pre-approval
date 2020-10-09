22c2a2c45e64f8325fd64189b1ed79eed51617fb
Ratanont Sukapan <ratanont@owndays-tech.com>
2020-10-09 09:31:28 +0700

    Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
 $(function () {
     // Add smooth scrolling to all links
-    $("a").on('click', function (event) {
+    $("a.link-scroll").on('click', function (event) {
 
         // Make sure this.hash has a value before overriding default behavior
         if (this.hash !== "") {
